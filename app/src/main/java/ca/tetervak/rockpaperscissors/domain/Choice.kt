6de8438f57726b136f5b70305ed48ac5824cdc4b
Alex Tetervak <tetervak@sheridancollege.ca>
2026-04-27 16:09:24 -0400

    copied domain classes

---
+++ app/src/main/java/ca/tetervak/rockpaperscissors/domain/Choice.kt
@@ -0,0 +1,3 @@
+package ca.tetervak.rockpaperscissors.domain
+
+enum class Choice { PAPER, ROCK, SCISSORS, UNKNOWN }
---
+++ app/src/main/java/ca/tetervak/rockpaperscissors/domain/GameResult.kt
@@ -0,0 +1,3 @@
+package ca.tetervak.rockpaperscissors.domain
+
+enum class GameResult { USER_WINS, COMPUTER_WINS, REPLAY, UNKNOWN }
