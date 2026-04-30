3efa6298db6eafb578d04526a6906147df80d897
Alex Tetervak <tetervak@sheridancollege.ca>
2026-04-30 12:07:24 -0400

    simplified the navigation

--- app/src/main/java/ca/tetervak/rockpaperscissors/ui/AppRootScreen.kt
+++ app/src/main/java/ca/tetervak/rockpaperscissors/ui/AppRootScreen.kt
@@ -3,36 +3,29 @@ package ca.tetervak.rockpaperscissors.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSerializable
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import ca.tetervak.rockpaperscissors.ui.common.AboutDialog
 import ca.tetervak.rockpaperscissors.ui.play.PlayScreen
 import ca.tetervak.rockpaperscissors.ui.result.ResultScreen
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.serializer
 
 
 @Serializable
-sealed interface GameNavKey : NavKey
+data object Play : NavKey
 
 @Serializable
-data object Play : GameNavKey
-
-@Serializable
-data object Result : GameNavKey
+data object Result : NavKey
 
 @Composable
 fun AppRootScreen(viewModel: GameViewModel = viewModel()) {
 
     val uiState: GameUiState by viewModel.gameUiState.collectAsState()
 
-    val gameNavBackStack = rememberSerializable(serializer = serializer()) {
-        NavBackStack<GameNavKey>(Play)
-    }
+    val gameNavBackStack = rememberNavBackStack(Play)
 
     NavDisplay(
         backStack = gameNavBackStack,
