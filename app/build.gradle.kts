1e82926e7d0d9ff8b2f9ed88d5a336223626adf5
Alex Tetervak <tetervak@sheridancollege.ca>
2026-09-14 14:51:54 -0400

    updated lib versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "ca.tetervak.rockpaperscissors"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
