22f3484d5076d076fdc65ef347be41528bcbb52e
Alex Tetervak <tetervak@sheridancollege.ca>
2026-09-22 09:58:20 -0400

    edited build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "ca.tetervak.rockpaperscissors"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "ca.tetervak.rockpaperscissors"
         minSdk = 34
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
