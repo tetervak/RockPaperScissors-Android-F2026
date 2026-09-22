9ecf0f68708bebd5753e6f42e03fafb256e784cf
Alex Tetervak <tetervak@sheridancollege.ca>
2026-09-22 09:48:19 -0400

    edited app build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "ca.tetervak.rockpaperscissors"
         minSdk = 34
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
